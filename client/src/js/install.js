8b22bde9cefa70ea5b81d6dfb5bf848beab864e7
MitchH10 <mjh10@live.com>
2023-03-07 22:28:34 -0600

    initial commit

---
+++ client/src/js/install.js
@@ -0,0 +1,19 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+
+    
+});
+butInstall.addEventListener('click', async () => {
+        butInstall.setAttribute('disabled', true);
+        console.log('Installed');
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+});
